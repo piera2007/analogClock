42ea9e9d4460b9dd9f36d5d242b73621f054fc9a
Piera <pieramblum@gmail.com>
2023-11-22 15:17:41 +0100

    noch hintergrund + römische zahlen verbessert

--- analogClock.js
+++ analogClock.js
@@ -15,3 +15,45 @@ function updateClock() {
   }
   setInterval(updateClock, 1000); // Update every second
   updateClock(); // Initial update
+
+  function addMarkings() {
+    const innerClockFace = document.querySelector('.inner-clock-face');
+  
+    // Füge Markierungen hinzu (Anpassen nach Bedarf)
+    for (let i = 5; i <= 60; i += 5) {
+      const marking = document.createElement('div');
+      marking.classList.add('marking');
+      marking.classList.add(`marking-${i}`);
+      marking.style.transform = `rotate(${(i / 60) * 360}deg)`;
+      innerClockFace.appendChild(marking);
+    }
+  }
+  
+  
+  // Führe die Funktion aus, wenn das DOM vollständig geladen ist
+  document.addEventListener('DOMContentLoaded', addMarkings);
+  function addCenterDot() {
+    const innerClockFace = document.querySelector('.inner-clock-face');
+  
+    // Erstelle das Element für den Mittelpunkt
+    const centerDot = document.createElement('div');
+    centerDot.classList.add('center-dot');
+  
+    // Füge es zum inneren Zifferblatt hinzu
+    innerClockFace.appendChild(centerDot);
+  }
+  
+  // Führe die Funktion aus, wenn das DOM vollständig geladen ist
+  document.addEventListener('DOMContentLoaded', addCenterDot);
+
+  // Array mit Bildern
+let bilderArray = ["bilda.png", "bildb.png", "bildc.png"];
+
+// Zufälliges Bild auswählen
+let zufallsIndex = Math.floor(Math.random() * bilderArray.length);
+let zufallsBild = bilderArray[zufallsIndex];
+
+// Setze den Hintergrund des .clock-Containers mit dem ausgewählten zufälligen Bildpfad
+const clockContainer = document.querySelector('.clock');
+clockContainer.style.backgroundImage = `url('${zufallsBild}')`;
+  
